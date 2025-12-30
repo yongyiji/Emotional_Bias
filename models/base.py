b27466eff201aec6f6b8a36664859cbbd3610d47
yongyiji <132156140+yongyiji@users.noreply.github.com>
2025-12-30 09:35:27 +0000

    Delete models directory

--- models/base.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class BaseModel(ABC):
-    @abstractmethod
-    def generate(self, prompts, n_samples=1, use_cot=False, use_fewshot=False, dataset_name=None):
-        pass
